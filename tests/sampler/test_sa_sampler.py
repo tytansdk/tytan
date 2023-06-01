be6b3a817c752b98bcd753b334c141710b647bab
Takashi Mitsuta <knhn1117@gmail.com>
2023-06-01 15:28:32 +0900

    Add seed to SASampler and GASampler.

--- tests/sampler/test_sa_sampler.py
+++ tests/sampler/test_sa_sampler.py
@@ -17,3 +17,16 @@ def test_sa_sampler_run():
     assert result[0][0]["z"] is not None
     assert result[0][1] is not None
     assert result[0][2] is not None
+
+
+def test_sa_sampler_run_with_seed():
+    x, y, z = sym.symbols("x y z")
+    expr = (x - y) ** 2 + (y - z) ** 2
+    Q, _offset = qubo.Compile(expr).get_qubo()
+    sampler = SASampler(seed=0)
+    result = sampler.run(qubo=Q, shots=1)
+    assert result is not None
+    assert result[0][0] is not None
+    assert result[0][0]["x"] == 0
+    assert result[0][0]["y"] == 0
+    assert result[0][0]["z"] == 0
