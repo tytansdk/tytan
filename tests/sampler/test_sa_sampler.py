22f34bf9ca670ebe4045e9132e09d5bc7b6d7bfc
YasuD <43514999+ShoyaYasuda@users.noreply.github.com>
2023-08-31 16:32:38 +0900

    Update test_sa_sampler.py

--- tests/sampler/test_sa_sampler.py
+++ tests/sampler/test_sa_sampler.py
@@ -4,10 +4,12 @@ from tytan.sampler import SASampler
 
 def test_sa_sampler_run():
     x, y, z = symbols("x y z")
-    expr = 3 * x**2 + 2 * x * y + 4 * y**2 + z**2 + 2 * x * z + 2 * y * z
+    expr = (x + y + z - 2)**2
     qubo, offset = Compile(expr).get_qubo()
     sampler = SASampler()
-    result = sampler.run(qubo, shots=1)
+    result = sampler.run(qubo)
+    for r in result:
+        print(r)
     assert result is not None
     assert result[0][0] is not None
     assert result[0][0]["x"] is not None
@@ -18,12 +20,26 @@ def test_sa_sampler_run():
 
 def test_sa_sampler_run_with_seed():
     x, y, z = symbols("x y z")
-    expr = (x - y) ** 2 + (y - z) ** 2
+    expr = (x + y + z - 2)**2
     qubo, offset = Compile(expr).get_qubo()
+    
+    #1
+    print('try 1, ', end='')
     sampler = SASampler(seed=0)
-    result = sampler.run(qubo, shots=1)
-    assert result is not None
-    assert result[0][0] is not None
-    assert result[0][0]["x"] == 0
-    assert result[0][0]["y"] == 0
-    assert result[0][0]["z"] == 0
+    result = sampler.run(qubo)
+    print(result[0][0]["x"], result[0][0]["y"], result[0][0]["z"], result[0][2])
+    x = result[0][0]["x"]
+    y = result[0][0]["y"]
+    z = result[0][0]["z"]
+    count = result[0][2]
+    
+    #2-
+    for i in range(2, 10):
+        print(f'try {i}, ', end='')
+        sampler = SASampler(seed=0)
+        result = sampler.run(qubo)
+        print(result[0][0]["x"], result[0][0]["y"], result[0][0]["z"], result[0][2])
+        assert result[0][0]["x"] == x
+        assert result[0][0]["y"] == y
+        assert result[0][0]["z"] == z
+        assert result[0][2] == count
