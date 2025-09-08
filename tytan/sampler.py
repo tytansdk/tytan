a265543839bc6ec6c681868491b14647713857df
YasuD <43514999+ShoyaYasuda@users.noreply.github.com>
2025-09-08 23:03:53 +0900

    numpy2系で結果がnp.int64(0)のように表示されることを解消

--- tytan/sampler.py
+++ tytan/sampler.py
@@ -11,22 +11,28 @@ pool=(shots, N), score=(N, )
 """
 def get_result(pool, score, index_map):
     #重複解を集計
+    # print(pool, index_map)
     unique_pool, original_index, unique_counts = np.unique(pool, axis=0, return_index=True, return_counts=True)
-    #print(unique_pool, original_index, unique_counts)
+    # print(unique_pool, original_index, unique_counts)
     
     #エネルギーもユニークに集計
     unique_energy = score[original_index]
-    #print(unique_energy)
+    # print(unique_energy)
     
     #エネルギー低い順にソート
     order = np.argsort(unique_energy)
     unique_pool = unique_pool[order]
     unique_energy = unique_energy[order]
     unique_counts = unique_counts[order]
+    # print(unique_pool)
     
     #結果リスト
-    result = [[dict(zip(index_map.keys(), unique_pool[i])), unique_energy[i], unique_counts[i]] for i in range(len(unique_pool))]
-    #print(result)
+    # result = [[dict(zip(index_map.keys(), unique_pool[i])), unique_energy[i], unique_counts[i]] for i in range(len(unique_pool))]
+    #numpy2.xでnp.int64(0)のように表示されることへの対応
+    result = [
+        [dict(zip(index_map.keys(), unique_pool[i].tolist())), float(unique_energy[i]), int(unique_counts[i])]
+        for i in range(len(unique_pool))
+        ]
     
     return result
 
